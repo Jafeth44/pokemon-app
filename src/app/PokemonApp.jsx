e76831d6f8cd2a0e23504aa4056784ab962d0e31
Bayron Ocampo <jafethocampo@icloud.com>
2023-09-17 22:14:43 -0600

    se agrego thumbnails de regiones

--- src/app/PokemonApp.jsx
+++ src/app/PokemonApp.jsx
@@ -1,9 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Box, Toolbar } from "@mui/material";
 import { Footer } from "./components/Footer";
 
 export const PokemonApp = () => {
+  const navigation = useNavigation();
   return (
     <>
       <Navbar />
@@ -15,7 +16,11 @@ export const PokemonApp = () => {
         justifyContent={"center"}
         alignItems={"center"}
         sx={{minHeight: {xs: 'calc(100dvh - 106px)', sm: 'calc(100dvh - 114px)'}}}>
-        <Outlet />
+          {
+            navigation.state == 'loading'
+            ? <h1>Loading...</h1>
+            : <Outlet />
+          }
         {/* dentro de este componente se van a rutear los componentes hijos */}
       </Box>
       <Footer />
