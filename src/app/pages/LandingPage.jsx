04946db6a902f2696ae866189882a6d857d308f8
Jean Carlo Arroyo <jeancarlo.arroyo@ayahealthcare.com>
2023-09-23 18:03:45 -0600

    new landing page and search bar for regions

--- src/app/pages/LandingPage.jsx
+++ src/app/pages/LandingPage.jsx
@@ -1,55 +1,131 @@
 import {
   Grid,
   Typography,
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
+  Box,
+  ButtonBase
 } from "@mui/material";
+import { styled } from '@mui/material/styles'
 import regions from "../../data/models/regions.json";
 import { useNavigate } from "react-router-dom";
 
 export const LandingPage = () => {
   const navigate = useNavigate();
 
+  const ImageButton = styled(ButtonBase)(({ theme }) => ({
+    position: 'relative',
+    height: 300,
+    margin: 18,
+    [theme.breakpoints.down('sm')]: {
+      width: '100% !important',
+      height: 100
+    },
+    '&:hover, &.Mui-focusVisible': {
+      zIndex: 1,
+      '& .MuiImageBackdrop-root': {
+        opacity: 0,
+      },
+      '& .MuiImageMarked-root': {
+        opacity: 0,
+      },
+      '& .MuiTypography-root': {
+        border: '4px solid currentColor'
+      },
+    },
+  }));
+
+  const ImageSrc = styled('span')({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    margin: 1,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  });
+
+  const Image = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  }));
+
+  const ImageBackdrop = styled('span')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    margin: 1,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.2,
+    transition: theme.transitions.create('opacity'),
+  }));
+
+  const ImageMarked = styled('span')(({ theme }) => ({
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  }));
+
   return (
     <>
       <Grid
         container
-        spacing={{ xs: 2, sm: 2 }}
-        p={{ xs: 2, sm: 2 }}
+        spacing={{ xs: 1, sm: 1 }}
+        p={{ xs: 1, sm: 1 }}
         justifyContent={"center"}
         maxWidth={"1400px"}>
-        <Grid item xs={12}>
-          <Typography variant="h4" textAlign={"center"}>
+        <Grid item xs={12} marginBottom={2}>
+          <Typography variant="h3" textAlign={"center"}>
             ¡Bienvenido a la Pokémon App!
           </Typography>
 
-          <Typography variant="subtitle1" textAlign={"center"}>
+          <Typography variant="h6" textAlign={"center"}>
             Selecciona tu región favorita para ver todos los pokémons
           </Typography>
         </Grid>
         {regions.map((region) => (
           <Grid item key={region.id} xs={12} sm={6} md={3} lg={12 / 5}>
-            <Card sx={{ boxShadow: 3 }}>
-              <CardActionArea onClick={() => navigate(`/region/${region.id}`)}>
-                <CardMedia
-                  component={"img"}
-                  height={{ xs: "140", sm: "240" }}
-                  image={region.image}
-                  alt={`image of the region ${region.name}`}
-                />
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%'}}>
+              <ImageButton
+                focusRipple
+                key={region.name}
+                style={{
+                  width: '95%'
+                }}
+                onClick={() => navigate(`/region/${region.id}`)}>
 
-                <CardContent>
+                <ImageSrc style={{backgroundImage: `url(${region.image})`}}/>
+                <ImageBackdrop className="MuiImageBackdrop-root"/>
+                <Image>
                   <Typography
-                    variant="h6"
-                    fontSize={{ xs: "1.1rem", sm: "1.3rem" }}
-                    textAlign={"center"}>
+                    component='span'
+                    variant="h5"
+                    color='inherit'
+                    sx={{
+                      position: 'relative',
+                      p: 4,
+                      pt: 2,
+                      pb: (theme) => `calc(${theme.spacing(1)})`,
+                    }}>
                     {region.name}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+
+                    <ImageMarked className="MuiImageMarked-root"/>
+                  </Typography>                
+                </Image> 
+              </ImageButton>
+            </Box>
           </Grid>
         ))}
       </Grid>
--- src/app/pages/RegionPage.jsx
+++ src/app/pages/RegionPage.jsx
@@ -1,40 +1,76 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
-import {Card, CardActionArea, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Grid, IconButton, Typography } from "@mui/material";
+import Paper from '@mui/material/Paper';
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from '@mui/icons-material/Search'
+import { useState } from "react";
 
 export const RegionPage = () => {
   const { name, pokemonEntries } = useLoaderData();
   const navigate = useNavigate();
-
+  const [searchName, setSearchName] = useState('');
+  const filterPoke = pokemonEntries.filter((poke) => poke.name.toLowerCase().includes(searchName.toLowerCase()));
+  const handleSearch = (e) => {setSearchName(e.target.value)};
 
   return (
     <>
       <Grid
         container
+        spacing={2}
+        p={2}
+        maxWidth={"1400px"}
+        justifyContent="space-between"
+        alignItems="center"
+        marginTop={1}
+      >
+        <Grid item>
+          <Typography variant="h4" textTransform="capitalize">{name}</Typography>
+        </Grid>
+        <Grid item>
+          <Paper
+            component='form'
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Buscar por Pokémon"
+              inputProps={{ 'aria-label': 'Buscar por Pokémon' }}
+              value={searchName}
+              onChange={handleSearch}
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
         spacing={{ xs: 2, sm: 2 }}
         p={{ xs: 2, sm: 2 }}
-        maxWidth={"1400px"}>
-          <Grid item xs={12}>
-            <Typography variant="h4" textTransform={"capitalize"}>{name}</Typography>
-          </Grid>
-        {pokemonEntries.map((poke) => (
+        maxWidth={"1400px"}
+      >
+        {filterPoke.map((poke) => (
           <Grid item key={poke.id} xs={4} sm={3} md={2} lg={2}>
-            <Card sx={{boxShadow: 3}}>
-              <CardActionArea onClick={() => navigate(`/pokemon/${poke.id}`, {relative: true})}>
+            <Card sx={{ boxShadow: 3 }}>
+              <CardActionArea onClick={() => navigate(`/pokemon/${poke.id}`, { relative: true })}>
                 <CardMedia
-                  component={"img"}
+                  component="img"
                   height={{ xs: "140", sm: "240" }}
                   image={poke.img}
                   alt={`image of the pokémon ${poke.name}`}
                 />
-              <CardContent sx={{p: '0'}}>
-                <Typography
-                  variant="h6"
-                  fontSize={{ xs: "1.1rem", sm: "1.3rem" }}
-                  textTransform={"capitalize"}
-                  textAlign={"center"}>
-                  {poke.name}
-                </Typography>
-              </CardContent>
+                <CardContent sx={{ p: '0' }}>
+                  <Typography
+                    variant="h6"
+                    fontSize={{ xs: "1.1rem", sm: "1.3rem" }}
+                    textTransform="capitalize"
+                    textAlign="center"
+                  >
+                    {poke.name}
+                  </Typography>
+                </CardContent>
               </CardActionArea>
             </Card>
           </Grid>
